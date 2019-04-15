7209320620be6bfb2b31f4c1520b51c56a0551b4
fazanki <fazanki@gmail.com>
2019-04-15 10:05:31 +0100

    adding router store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './containers/app/app.component';
 
+import {environment} from '../environments/environment';
+import {CustomSerializer, reducers} from './store/reducers';
+import {effects} from './store/effects';
+
+import {storeFreeze} from 'ngrx-store-freeze';
+import {MetaReducer, StoreModule} from '@ngrx/store';
+import {RouterStateSerializer, StoreRouterConnectingModule} from '@ngrx/router-store';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+
+export const metaReducers: MetaReducer<any>[] = !environment.production
+  ? [storeFreeze]
+  : [];
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    EffectsModule.forRoot(effects),
+    StoreRouterConnectingModule,
+    StoreDevtoolsModule.instrument({
+      logOnly: environment.production
+    }),
   ],
-  providers: [],
+  providers: [{ provide: RouterStateSerializer, useClass: CustomSerializer }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
